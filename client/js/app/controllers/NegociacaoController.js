01cf511d7a7ebfe3a118fd81d3e6aa6e26a9bb53
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-08-08 20:59:18 -0300

    Created a view for the project.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,12 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes();
 
-    this._limpaFormulario();
+    this._listaNegociacoes = new ListaNegociacoes();
+    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+    this._negociacoesView.update(this._listaNegociacoes);
+
+
   }
   
   adiciona(event) {
